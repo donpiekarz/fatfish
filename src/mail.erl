db41e5a2d54d15da348030a1d4e485843baaa384
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2014-10-22 05:48:08 -0400

    FEAT attachment support by mail module

--- src/mail.erl
+++ src/mail.erl
@@ -5,7 +5,7 @@
 
 
 compose_mail(Options)->
-    Fields = [from, to, subject, body],
+    Fields = [from, to, subject, body, body_mime],
     compose_mail(Fields, Options, "").
 
 compose_mail([Field], Options, Output)->
@@ -30,6 +30,46 @@ dispatch_field({to, Value}, Output) ->
 dispatch_field({subject, Value}, Output) ->
     Output ++ "Subject: " ++ Value ++ "\r\n";
 dispatch_field({body, Value}, Output) ->
-    Output ++ "\r\n" ++ Value.
+    Output ++ "\r\n" ++ Value;
+dispatch_field({body_mime, Value}, Output) ->
+    compose_mime_field(Value, Output).
 
+compose_mime_field(Options, Output)->
+    {separator, Separator} = lists:keyfind(separator, 1, Options),
+    NewOptions = lists:keydelete(separator, 1, Options),
+    OutputAfterInit = dispatch_mime_field(init, Separator, Output),
+    OutputAfterDisp = dispatch_mime_field(NewOptions, Separator, OutputAfterInit),
+    dispatch_mime_field(close, Separator, OutputAfterDisp).
 
+dispatch_mime_field(init, Separator, Output)->
+    Output ++ "\r\nContent-Type: multipart/mixed; boundary=\"" ++ Separator ++ "\"\r\n";
+dispatch_mime_field(open, Separator,  Output)->
+    Output ++ "\r\n--" ++ Separator ++ "\r\n";
+dispatch_mime_field(close, Separator,  Output)->
+    Output ++ "\r\n--" ++ Separator ++ "--" ++ "\r\n";
+dispatch_mime_field({body, Value}, Separator, Output)->
+    dispatch_mime_field(open, Separator, Output) ++ Value ++ "\r\n\r\n";
+dispatch_mime_field({attchment, Value}, Separator, Output)->
+    Encoding = lists:keyfind(content_transfer_encoding,1 , Value),
+    Type = lists:keyfind(content_type, 1, Value),
+    Name = lists:keyfind(name, 1, Value),
+    Data = lists:keyfind(data, 1, Value),
+    dispatch_mime_field(open, Separator, Output) ++
+        dispatch_mime_field_attachment(Encoding) ++ 
+        dispatch_mime_field_attachment(Type) ++ 
+        dispatch_mime_field_attachment(Name) ++ 
+        dispatch_mime_field_attachment(Data);
+dispatch_mime_field([Field], Separator, Output) ->
+    dispatch_mime_field(Field, Separator, Output);
+dispatch_mime_field([Field|Fields], Separator, Output) ->
+    NewOutput = dispatch_mime_field(Field, Separator, Output),
+    dispatch_mime_field(Fields, Separator, NewOutput).
+
+dispatch_mime_field_attachment({content_transfer_encoding, Value}) ->
+    "Content-transfer-encoding: " ++ Value ++ ";\r\n";
+dispatch_mime_field_attachment({content_type, Value}) ->
+    "Content-Type: " ++ Value ++ "; ";
+dispatch_mime_field_attachment({name, Value}) ->
+    "name=\"" ++ Value ++ "\"" ++ "\r\n";
+dispatch_mime_field_attachment({data, Value}) ->
+    "\r\n" ++ Value ++ "\r\n".
--- test/mail_tests.erl
+++ test/mail_tests.erl
@@ -13,3 +13,25 @@ compose_mail_test()->
     Expect = "From: <aaa@bbb.com>\r\nTo: <ccc@ddd.com>\r\nSubject: testing\r\n\r\nThis is the email body",
     Actual = mail:compose_mail(Mail),
     ?assertEqual(Expect, Actual).
+
+ compose_mail_mime_test()->
+    Mail = [
+        {from, "<aaa@bbb.com>"},
+        {to, "<ccc@ddd.com>"},
+        {subject, "testing"},
+        {body_mime, 
+            [
+                {separator, "000SomeRandomString000"},
+                {body, "Here's a picture of me"},
+                {attchment, [
+                    {content_transfer_encoding, "base64"},
+                    {content_type, "application/octet-stream"},
+                    {name, "PictureOfMe.png"},
+                    {data, "blablabla"}
+                    ]}
+            ]
+        }
+    ],
+    Expect = "From: <aaa@bbb.com>\r\nTo: <ccc@ddd.com>\r\nSubject: testing\r\n\r\nContent-Type: multipart/mixed; boundary=\"000SomeRandomString000\"\r\n\r\n--000SomeRandomString000\r\nHere's a picture of me\r\n\r\n\r\n--000SomeRandomString000\r\nContent-transfer-encoding: base64;\r\nContent-Type: application/octet-stream; name=\"PictureOfMe.png\"\r\n\r\nblablabla\r\n\r\n--000SomeRandomString000--\r\n",
+    Actual = mail:compose_mail(Mail),
+    ?assertEqual(Expect, Actual).
