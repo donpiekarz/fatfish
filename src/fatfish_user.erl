a08f23d735d4ec5bd367ee59d7f14b913e600292
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2014-12-31 16:41:50 +0100

    fix user data

--- src/fatfish_user.erl
+++ src/fatfish_user.erl
@@ -3,14 +3,12 @@
 -export([get_to/1, get_cert/1]).
 
 get_to(<<"koparka.czerwona@fatfish.pepiniera.net">>) ->
-    <<"koparka.czerwona@gmail.com">>.
-
+    <<"koparka.czerwona@gmail.com">>;
 get_to(<<"koparka.niebieska@fatfish.pepiniera.net">>) ->
     <<"koparka.niebieska@interia.pl">>.
 
 get_cert(<<"koparka.czerwona@gmail.com">>) ->
-    load_cert("/certs/koparka.czerwona.cert.pem").
-
+    load_cert("/certs/koparka.czerwona.cert.pem");
 get_cert(<<"koparka.niebieska@interia.pl">>) ->
     load_cert("/certs/koparka.niebieska.cert.pem").
 
